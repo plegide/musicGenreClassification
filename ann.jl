7fc25dbafcb5b6a22d17cd34b4f0615b0015527d
juaandominguez <juan.dominguezr@udc.es>
2024-03-16 12:37:02 +0100

    feat: normalization & training

--- ann.jl
+++ ann.jl
@@ -1,4 +1,4 @@
-using Flux, Flux.Losses, DelimitedFiles, Plots, ProgressMeter
+using Flux, Flux.Losses, DelimitedFiles, Plots, ProgressMeter, Random
 
 
 function oneHotEncoding(feature::AbstractArray{<:Any,1}, classes::AbstractArray{<:Any,1})
@@ -16,6 +16,56 @@ function oneHotEncoding(feature::AbstractArray{<:Any,1}, classes::AbstractArray{
     end
     return feature
 end;
+
+function calculateMinMaxNormalizationParameters(data::AbstractArray{<:Real,2})
+    mins = minimum(data, dims=1)
+    maxs = maximum(data, dims=1)
+    return (mins, maxs)
+end
+
+function calculateZeroMeanNormalizationParameters(data::AbstractArray{<:Real,2})
+    means = mean(data, dims=1)
+    stds = std(data, dims=1)
+    return (means, stds)
+end
+
+function holdOut(N::Int, P::Float64)
+    @assert ((P>=0.) & (P<=1.));
+    indices = randperm(N);
+    numTrainingInstances = Int(round(N*(1-P)));
+    return (indices[1:numTrainingInstances], indices[numTrainingInstances+1:end]);
+end
+
+function holdOut(N::Int, Pval::Float64, Ptest::Float64)
+    @assert ((Pval>=0.) & (Pval<=1.));
+    @assert ((Ptest>=0.) & (Ptest<=1.));
+    @assert ((Pval+Ptest)<=1.);
+    # Primero separamos en entrenamiento+validation y test
+    (trainingValidationIndices, testIndices) = holdOut(N, Ptest);
+    # Después separamos el conjunto de entrenamiento+validation
+    (trainingIndices, validationIndices) = holdOut(length(trainingValidationIndices), Pval*N/length(trainingValidationIndices))
+    return (trainingValidationIndices[trainingIndices], trainingValidationIndices[validationIndices], testIndices);
+end;
+
+function normalizeMinMax!(dataset::AbstractArray{<:Real,2}, normalizationParameters::NTuple{2, AbstractArray{<:Real,2}})
+    mins, maxs = normalizationParameters
+    dataset .= (dataset .- mins) ./ (maxs .- mins)
+end
+
+function normalizeMinMax!(dataset::AbstractArray{<:Real,2})
+    normalizationParameters = calculateMinMaxNormalizationParameters(dataset)
+    normalizeMinMax!(dataset, normalizationParameters)
+end
+
+function normalizeMinMax( dataset::AbstractArray{<:Real,2}, normalizationParameters::NTuple{2, AbstractArray{<:Real,2}})
+    mins, maxs = normalizationParameters
+    return (dataset .- mins) ./ (maxs .- mins)
+end
+
+function normalizeMinMax(dataset::AbstractArray{<:Real,2})
+    normalizationParameters = calculateMinMaxNormalizationParameters(dataset)
+    return normalizeMinMax(dataset, normalizationParameters)
+end
 
 function accuracy(outputs::AbstractArray{Bool,1}, targets::AbstractArray{Bool,1})
     return mean(outputs .== targets)
@@ -75,51 +125,63 @@ function buildClassANN(numInputs::Int, topology::AbstractArray{<:Int,1}, numOutp
     return ann
 end
 
-function trainClassANN(topology::AbstractArray{<:Int,1},
-    dataset::Tuple{AbstractArray{<:Real,2},AbstractArray{Bool,2}};
-    transferFunctions::AbstractArray{<:Function,1}=fill(σ, length(topology)),
-    maxEpochs::Int=1000, minLoss::Real=0.0, learningRate::Real=0.01)
-
-    inputs, targets = dataset
-
-    # Crear una RNA usando la función buildClassANN
+function trainClassANN(topology::AbstractArray{<:Int,1}, trainingDataset::Tuple{AbstractArray{<:Real,2}, AbstractArray{Bool,2}}; validationDataset::Tuple{AbstractArray{<:Real,2}, AbstractArray{Bool,2}}= (Array{eltype(trainingDataset[1]),2}(undef,0,0), falses(0,0)), testDataset::Tuple{AbstractArray{<:Real,2}, AbstractArray{Bool,2}}= (Array{eltype(trainingDataset[1]),2}(undef,0,0), falses(0,0)), transferFunctions::AbstractArray{<:Function,1}=fill(σ, length(topology)), maxEpochs::Int=1000, minLoss::Real=0.0, learningRate::Real=0.01, maxEpochsVal::Int=20)
+    inputs, targets = trainingDataset
     ann = buildClassANN(size(inputs, 2), topology, size(targets, 2), transferFunctions=transferFunctions)
-
     losses = []  # Lista para almacenar los valores de pérdida en cada ciclo
-
-    # Configurar el optimizador y el estado inicial
     opt_state = Flux.setup(Adam(learningRate), ann)
-
-    # Bucle de entrenamiento
+    bestValLoss = Inf
+    bestValLossEpoch = 0
+    bestAnn = ann
     @showprogress for epoch in 1:maxEpochs
-        # Realizar una iteración de entrenamiento
         Flux.train!(loss, ann, [(inputs', targets')], opt_state)
-
-        # Calcular la pérdida en esta época y almacenarla en la lista
-        loss_value = loss(ann, inputs', targets')  # se tranponen las matrices por convencion de la RR.NN.AA
+        loss_value = loss(ann, inputs', targets')
         push!(losses, loss_value)
-
-        # Verificar si se ha alcanzado la pérdida mínima
         if loss_value ≤ minLoss
             println("Pérdida mínima alcanzada después de $epoch ciclos.")
             break
         end
-
+        if epoch % maxEpochsVal == 0
+            valLoss = loss(ann, validationDataset...)
+            if valLoss < bestValLoss
+                bestValLoss = valLoss
+                bestValLossEpoch = epoch
+                bestAnn = ann
+            end
+        end
     end
-    return ann, losses
+    return bestAnn, losses, bestValLoss, bestValLossEpoch
 end
 
-
-function trainClassANN(topology::AbstractArray{<:Int,1},
-    (inputs, targets)::Tuple{AbstractArray{<:Real,2},AbstractArray{Bool,1}};
-    transferFunctions::AbstractArray{<:Function,1}=fill(σ, length(topology)),
-    maxEpochs::Int=1000, minLoss::Real=0.0, learningRate::Real=0.01)
-
-    dataset = (inputs, reshape(targets, :, 1))
-    return trainClassANN(topology, dataset, transferFunctions=transferFunctions,
-        maxEpochs=maxEpochs, minLoss=minLoss, learningRate=learningRate)
+function trainClassANN(topology::AbstractArray{<:Int,1}, trainingDataset::Tuple{AbstractArray{<:Real,2}, AbstractArray{Bool,1}}; validationDataset::Tuple{AbstractArray{<:Real,2}, AbstractArray{Bool,1}}= (Array{eltype(trainingDataset[1]),2}(undef,0,0), falses(0)), testDataset::Tuple{AbstractArray{<:Real,2}, AbstractArray{Bool,1}}= (Array{eltype(trainingDataset[1]),2}(undef,0,0), falses(0)), transferFunctions::AbstractArray{<:Function,1}=fill(σ, length(topology)), maxEpochs::Int=1000, minLoss::Real=0.0, learningRate::Real=0.01, maxEpochsVal::Int=20)
+    inputs, targets = trainingDataset
+    ann = buildClassANN(size(inputs, 2), topology, 1, transferFunctions=transferFunctions)
+    losses = []  # Lista para almacenar los valores de pérdida en cada ciclo
+    opt_state = Flux.setup(Adam(learningRate), ann)
+    bestValLoss = Inf
+    bestValLossEpoch = 0
+    bestAnn = ann
+    @showprogress for epoch in 1:maxEpochs
+        Flux.train!(loss, ann, [(inputs', targets')], opt_state)
+        loss_value = loss(ann, inputs', targets')
+        push!(losses, loss_value)
+        if loss_value ≤ minLoss
+            println("Pérdida mínima alcanzada después de $epoch ciclos.")
+            break
+        end
+        if epoch % maxEpochsVal == 0
+            valLoss = loss(ann, validationDataset...)
+            if valLoss < bestValLoss
+                bestValLoss = valLoss
+                bestValLossEpoch = epoch
+                bestAnn = ann
+            end
+        end
+    end
+    return bestAnn, losses, bestValLoss, bestValLossEpoch
 end
 
+# 1. Cargar el conjunto de datos
 dataset = readdlm("classicOrMetal.data",',');
 inputs = dataset[:,1:2];
 targets = dataset[:,3];
@@ -128,4 +190,43 @@ classes = unique(targets);
 inputs = convert(Array{Float32,2},inputs);
 targets = oneHotEncoding(targets, classes);
 
-ann, losses = trainClassANN([2, 3, 2], (inputs, targets), maxEpochs=1000, minLoss=0.01, learningRate=0.01)
+# 2. Separar el conjunto de datos en entrenamiento, validacion y prueba
+(trainIndexes, validationIndexes, testIndexes) = holdOut(size(inputs,1), 0.2, 0.2);
+
+trainInputs = inputs[trainIndexes,:];
+validationInputs = inputs[validationIndexes,:];
+testInputs = inputs[testIndexes,:];
+trainTargets = targets[trainIndexes,:];
+validationTargets = targets[validationIndexes,:];
+testTargets = targets[testIndexes,:];
+
+# 3. Calcular parametros de normalizacion
+normalizationParams = calculateMinMaxNormalizationParameters(trainInputs);
+
+# 4. Normalizar el conjunto de datos
+normalizeMinMax!(trainInputs, normalizationParams);
+normalizeMinMax!(validationInputs, normalizationParams);
+normalizeMinMax!(testInputs, normalizationParams);
+
+# 5. Entrenar la RNA
+topology = [2, 1];
+(ann, losses, valLoss, valLossEpoch) = trainClassANN(topology, (trainInputs', trainTargets'), validationDataset=(validationInputs', validationTargets'));
+# 6. Evaluar la RNA
+testLoss = loss(ann, testInputs', testTargets');
+testOutputs = ann(testInputs');
+testAccuracy = accuracy(testOutputs, testTargets);
+
+# 7. Mostrar resultados
+println("Pérdida de prueba: $testLoss")
+println("Precisión de prueba: $testAccuracy")
+
+plot(losses, label="Pérdida de entrenamiento")
+xlabel!("Ciclo")
+ylabel!("Pérdida")
+title!("Pérdida de entrenamiento durante el entrenamiento de la RNA")
+plot!([valLossEpoch], [valLoss], seriestype=:scatter, label="Pérdida de validación mínima")
+plot!([valLossEpoch], [valLoss], seriestype=:vline, label="Pérdida de validación mínima")
+plot!([valLossEpoch], [valLoss], seriestype=:hline, label="Pérdida de validación mínima")
+
+# 8. Guardar la RNA
+
