0bac108230a86ae1c535b564f305f512d1f1fb9f
Victor Badillo <victor.badillo@udc.es>
2024-03-26 17:48:44 +0100

    otros cambios

--- fonts/funciones.jl
+++ fonts/funciones.jl
@@ -190,6 +190,7 @@ function buildClassANN(numInputs::Int, topology::AbstractArray{<:Int,1}, numOutp
     return ann
 end
 
+
 function trainClassANN(topology::AbstractArray{<:Int,1}, trainingDataset::Tuple{AbstractArray{<:Real,2}, AbstractArray{Bool,2}}; validationDataset::Tuple{AbstractArray{<:Real,2}, AbstractArray{Bool,2}}= (Array{eltype(trainingDataset[1]),2}(undef,0,0), falses(0,0)), testDataset::Tuple{AbstractArray{<:Real,2}, AbstractArray{Bool,2}}= (Array{eltype(trainingDataset[1]),2}(undef,0,0), falses(0,0)), transferFunctions::AbstractArray{<:Function,1}=fill(σ, length(topology)), maxEpochs::Int=1000, minLoss::Real=0.0, learningRate::Real=0.01, maxEpochsVal::Int=20)
     inputs, targets = trainingDataset
     # ann = buildClassANN(size(inputs, 2), topology, size(targets, 2), transferFunctions=transferFunctions)
@@ -258,6 +259,36 @@ function trainClassANN(topology::AbstractArray{<:Int,1}, trainingDataset::Tuple{
     return bestAnn, losses, bestValLoss, bestValLossEpoch
 end
 
+
+
+
+ #=
+function trainClassANN(topology::AbstractArray{<:Int,1}, 
+    trainingDataset::Tuple{AbstractArray{<:Real,2}, AbstractArray{Bool,2}}; 
+    validationDataset::Tuple{AbstractArray{<:Real,2}, AbstractArray{Bool,2}}= 
+    (Array{eltype(trainingDataset[1]),2}(undef,0,0), falses(0,0)), 
+    testDataset::Tuple{AbstractArray{<:Real,2}, AbstractArray{Bool,2}}= 
+    (Array{eltype(trainingDataset[1]),2}(undef,0,0), falses(0,0)), 
+    transferFunctions::AbstractArray{<:Function,1}=fill(σ, length(topology)), 
+    maxEpochs::Int=1000, minLoss::Real=0.0, learningRate::Real=0.01, 
+    maxEpochsVal::Int=20)
+=#
+
+#=
+function trainClassANN(topology::AbstractArray{<:Int,1}, 
+trainingDataset::Tuple{AbstractArray{<:Real,2}, AbstractArray{Bool,1}}; 
+validationDataset::Tuple{AbstractArray{<:Real,2}, AbstractArray{Bool,1}}= 
+(Array{eltype(trainingDataset[1]),2}(undef,0,0), falses(0)), 
+testDataset::Tuple{AbstractArray{<:Real,2}, AbstractArray{Bool,1}}= 
+(Array{eltype(trainingDataset[1]),2}(undef,0,0), falses(0)), 
+transferFunctions::AbstractArray{<:Function,1}=fill(σ, length(topology)), 
+maxEpochs::Int=1000, minLoss::Real=0.0, learningRate::Real=0.01, 
+maxEpochsVal::Int=20)
+=#
+
+
+
+
 function confusionMatrix(outputs::AbstractArray{Bool,1}, targets::AbstractArray{Bool,1})
     VP = sum(outputs .& targets)
     VN = sum((.~outputs) .& (.~targets))
@@ -286,3 +317,168 @@ function printConfusionMatrix(outputs::AbstractArray{<:Real,1}, targets::Abstrac
     outputs_bool = classifyOutputs(outputs, threshold=threshold)
     printConfusionMatrix(outputs_bool, targets)
 end
+
+
+#=
+function calculateMetrics()
+    # Calculamos el loss en entrenamiento y test. Para ello hay que pasar las matrices traspuestas (cada patron en una columna)
+    trainingLoss = loss(trainingInputs', trainingTargets');
+    validationLoss = loss(validationInputs', validationTargets');
+    testLoss = loss(testInputs', testTargets');
+    # Calculamos la salida de la RNA en entrenamiento y test. Para ello hay que pasar la matriz de entradas traspuesta (cada patron en una columna). La matriz de salidas tiene un patron en cada columna
+    trainingOutputs = ann(trainingInputs');
+    validationOutputs = ann(validationInputs');
+    testOutputs = ann(testInputs');
+    # Para calcular la precision, ponemos 2 opciones aqui equivalentes:
+    # Pasar las matrices con los datos en las columnas. La matriz de salidas ya tiene un patron en cada columna
+    trainingAcc = accuracy(trainingOutputs, 
+   Array{Bool,2}(trainingTargets'); dataInRows=false);
+    validationAcc = accuracy(validationOutputs,
+   Array{Bool,2}(validationTargets'); dataInRows=false);
+    testAcc = accuracy(testOutputs, Array{Bool,2}(testTargets');
+    dataInRows=false);
+    # Pasar las matrices con los datos en las filas. Hay que trasponer la matriz de salidas de la RNA, puesto que cada dato esta en una fila
+    trainingAcc = accuracy(Array{Float64,2}(trainingOutputs'), 
+   trainingTargets; dataInRows=true);
+    validationAcc = accuracy(Array{Float64,2}(validationOutputs'), 
+   validationTargets; dataInRows=true);
+    testAcc = accuracy(Array{Float64,2}(testOutputs'), 
+   testTargets; dataInRows=true);
+    # Mostramos por pantalla el resultado de este ciclo de entrenamiento si nos lo han indicado
+    if showText
+    println("Epoch ", numEpoch, ": Training loss: ", trainingLoss, ", 
+   accuracy: ", 100*trainingAcc, " % - Validation loss: ", validationLoss, ", 
+   accuracy: ", 100*validationAcc, " % - Test loss: ", testLoss, ", accuracy: ", 
+   100*testAcc, " %");
+    end;
+    return (trainingLoss, trainingAcc, validationLoss, validationAcc, 
+   testLoss, testAcc)
+    end;
+   =#
+
+
+   function modelCrossValidation(modelType::Symbol, modelHyperparameters::Dict, 
+    inputs::Array{Float32,2}, targets::Array{Any,1}, numFolds::Int64)
+    
+        # Comprobamos que el numero de patrones coincide
+        @assert(size(inputs,1)==length(targets));
+    
+        # Que clases de salida tenemos
+        # Es importante calcular esto primero porque se va a realizar codificacion one-hot-encoding varias veces, y el orden de las clases deberia ser el mismo siempre
+        classes = unique(targets);
+    
+        # Primero codificamos las salidas deseadas en caso de entrenar RR.NN.AA.
+        if modelType==:ANN
+        targets = oneHotEncoding(targets, classes);
+        end;
+    
+        # Creamos los indices de crossvalidation
+        crossValidationIndices = crossvalidation(size(inputs,1), numFolds);
+    
+        # Creamos los vectores para las metricas que se vayan a usar
+        # En este caso, solo voy a usar precision y F1, en otro problema podrían ser distintas
+        testAccuracies = Array{Float64,1}(undef, numFolds);
+        testF1 = Array{Float64,1}(undef, numFolds);
+    
+        # Para cada fold, entrenamos
+        for numFold in 1:numFolds
+    
+            # Si vamos a usar unos de estos 3 modelos
+            if (modelType==:SVM) || (modelType==:DecisionTree) || (modelType==:kNN)
+    
+                # Dividimos los datos en entrenamiento y test
+                trainingInputs = inputs[crossValidationIndices.!=numFold,:];
+                testInputs = inputs[crossValidationIndices.==numFold,:];
+                trainingTargets = targets[crossValidationIndices.!=numFold];
+                testTargets = targets[crossValidationIndices.==numFold];
+    
+                if modelType==:SVM
+                    model = SVC(kernel=modelHyperparameters["kernel"], 
+                    degree=modelHyperparameters["kernelDegree"], 
+                    gamma=modelHyperparameters["kernelGamma"], C=modelHyperparameters["C"]);
+    
+                elseif modelType==:DecisionTree
+                    model = DecisionTreeClassifier(max_depth=modelHyperparameters["maxDepth"], random_state=1);
+                elseif modelType==:kNN
+                    model = KNeighborsClassifier(modelHyperparameters["numNeighbors"]);
+                end;
+     
+               # Entrenamos el modelo con el conjunto de entrenamiento
+                model = fit!(model, trainingInputs, trainingTargets);
+    
+                # Pasamos el conjunto de test
+                testOutputs = predict(model, testInputs);
+    
+                # Calculamos las metricas correspondientes con la funcion desarrollada en la practica anterior
+                (acc, _, _, _, _, _, F1, _) = confusionMatrix(testOutputs, testTargets);
+    
+            else
+    
+                # Vamos a usar RR.NN.AA.
+                @assert(modelType==:ANN);
+    
+                # Dividimos los datos en entrenamiento y test
+                trainingInputs = inputs[crossValidationIndices.!=numFold,:];
+                testInputs = inputs[crossValidationIndices.==numFold,:];
+                trainingTargets = targets[crossValidationIndices.!=numFold,:];
+                testTargets = targets[crossValidationIndices.==numFold,:];
+    
+                # Como el entrenamiento de RR.NN.AA. es no determinístico, hay que entrenar varias veces, y
+                # se crean vectores adicionales para almacenar las metricas para cada entrenamiento
+                testAccuraciesEachRepetition = Array{Float64,1}(undef, modelHyperparameters["numExecutions"]);
+                testF1EachRepetition = Array{Float64,1}(undef, modelHyperparameters["numExecutions"]);
+    
+                # Se entrena las veces que se haya indicado
+                for numTraining in 1:modelHyperparameters["numExecutions"]
+    
+                    if modelHyperparameters["validationRatio"]>0
+    
+                        # Para el caso de entrenar una RNA con conjunto de validacion, hacemos una división adicional:
+                        # dividimos el conjunto de entrenamiento en entrenamiento+validacion
+                        # Para ello, hacemos un hold out
+                        (trainingIndices, validationIndices) = holdOut(size(trainingInputs,1), modelHyperparameters["validationRatio"]*size(trainingInputs,1)/size(inputs,1));
+                        # Con estos indices, se pueden crear los vectores finales que vamos a usar para entrenar una RNA
+                        # Entrenamos la RNA, teniendo cuidado de codificar las salidas deseadas correctamente
+                        ann, = trainClassANN(modelHyperparameters["topology"],
+                                (trainingInputs', trainingTargets'),
+                                trainingInputs[validationIndices,:], 
+                                trainingTargets[validationIndices,:],
+                                testInputs, testTargets;
+                                maxEpochs=modelHyperparameters["maxEpochs"], 
+                                learningRate=modelHyperparameters["learningRate"], 
+                                maxEpochsVal=modelHyperparameters["maxEpochsVal"]);
+                    else
+    
+                        # Si no se desea usar conjunto de validacion, se entrena unicamente con conjuntos de entrenamiento y test,
+                        # teniendo cuidado de codificar las salidas deseadas correctamente
+                        ann, = trainClassANN(modelHyperparameters["topology"],
+                            (trainingInputs', trainingTargets'),
+                            testInputs, testTargets;
+                            maxEpochs=modelHyperparameters["maxEpochs"], 
+                            learningRate=modelHyperparameters["learningRate"]);
+                    end;
+    
+                    # Calculamos las metricas correspondientes con la funcion desarrollada en la practica anterior
+                    (testAccuraciesEachRepetition[numTraining], _, _, _, _, _, testF1EachRepetition[numTraining], _) = confusionMatrix(collect(ann(testInputs')'), testTargets);
+                end;
+    
+                # Calculamos el valor promedio de todos los entrenamientos de este fold
+    
+                acc = mean(testAccuraciesEachRepetition);
+                F1 = mean(testF1EachRepetition);
+            end;
+    
+            # Almacenamos las 2 metricas que usamos en este problema
+            testAccuracies[numFold] = acc;
+            testF1[numFold] = F1;
+    
+            println("Results in test in fold ", numFold, "/", numFolds, ": accuracy:", 100*testAccuracies[numFold], " %, F1: ", 100*testF1[numFold], " %");
+    
+        end; # for numFold in 1:numFolds
+    
+        println(modelType, ": Average test accuracy on a ", numFolds, "-fold crossvalidation: ", 100*mean(testAccuracies), ", with a standard deviation of ",100*std(testAccuracies));
+    
+        println(modelType, ": Average test F1 on a ", numFolds, "-fold crossvalidation: ", 100*mean(testF1), ", with a standard deviation of ", 100*std(testF1));
+    
+        return (mean(testAccuracies), std(testAccuracies), mean(testF1), std(testF1));
+    end;
