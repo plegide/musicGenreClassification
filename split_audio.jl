318bf6f7ca559ddc908da277bacd866ee355d61b
j.rrodriguez1 <j.rrodriguez1@udc.es>
2024-04-27 19:59:58 +0200

    Cambio split audio

--- split_audio.jl
+++ split_audio.jl
@@ -5,9 +5,8 @@ function dividir_audio(input_file::AbstractString, output_folder::AbstractString
     audio, sample_rate = wavread(input_file)
 
     total_samples = length(audio)
-    num_segments = ceil(Int, total_samples / (segment_length - overlap))
 
-    for i in 1:num_segments-1
+    for i in 1:5
         # Calcula el principio y el fin del segmento en concreto (hay overlap entre segmentos)
         start_index = (i - 1) * (segment_length - overlap) + 1
         end_index = min(start_index + segment_length - 1, total_samples)
@@ -23,16 +22,6 @@ function dividir_audio(input_file::AbstractString, output_folder::AbstractString
         # Guarda el segmento como un wav
         wavwrite(segment, output_file, Fs=sample_rate)
     end
-        # El mismo proceso pero hecho para el ultimo segmento (hay mas overlap entre el penultimo segmento y el ultimo que entre el resto)
-        start_index = (total_samples - segment_length) + 1
-        end_index = total_samples
-        segment = zeros(segment_length)
-        segment[1:end_index - start_index + 1] = audio[start_index:end_index]
-        if !isdir(output_folder)
-            mkdir(output_folder)
-        end
-        output_file = joinpath(output_folder, "$(basename(input_file))_segment_$(num_segments).wav")
-        wavwrite(segment, output_file, Fs=sample_rate)
 end
 
 println("[!] Processing audio files")
@@ -46,7 +35,7 @@ for genre in readdir("genres")
     end
     println("Procesando $(genre)...")
     for audio in readdir(joinpath("genres", genre))
-        if endswith(audio, ".wav")
+        if endswith(audio, ".wav") && !startswith(audio, "blues") && !startswith(audio, "pop") && !startswith(audio, "country")
             try
                 dividir_audio(joinpath("genres", genre, audio), joinpath("segments", genre), 65536, 16384)
             catch
